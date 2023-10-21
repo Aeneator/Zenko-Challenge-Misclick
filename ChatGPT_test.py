d6ca1dbf62ce2d8cff07bc4f695148127937daf5
Adrian Mihai <adi_pilescu@yahoo.ro>
2023-10-21 17:36:37 +0300

    Update ChatGPT_test.py

--- ChatGPT_test.py
+++ ChatGPT_test.py
@@ -3,25 +3,18 @@ import openai
 API_KEY = open("API_KEY.txt", 'r').read()
 openai.api_key = API_KEY
 
-output = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[{"role": "user",
-               "content": "What is google"}]
-)
-print(output)
+chat_log = []
 
-# chat_log = []
-#
-# while True:
-#     user_message = input()
-#     if user_message.lower() == "quit":
-#         break
-#     else:
-#         chat_log.append({"role": "user", "content": user_message})
-#         response = openai.ChatCompletion.create(
-#             model="gpt-3.5-turbo",
-#             messages=chat_log
-#         )
-#         assistant_response = response['choices'][0]['message']['content']
-#         print("ChatGPT:", assistant_response.strip("\n").strip())
-#         chat_log.append({"role": "assistant", "content": assistant_response.strip("\n").strip()})
+while True:
+    user_message = input()
+    if user_message.lower() == "quit":
+        break
+    else:
+        chat_log.append({"role": "user", "content": user_message})
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=chat_log
+        )
+        assistant_response = response['choices'][0]['message']['content']
+        print("ChatGPT:", assistant_response.strip("\n").strip())
+        chat_log.append({"role": "assistant", "content": assistant_response.strip("\n").strip()})
