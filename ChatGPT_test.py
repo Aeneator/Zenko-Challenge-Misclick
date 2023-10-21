5f6ff1707520e835def9f36cf9714be0cbc887d8
Adrian Mihai <adi_pilescu@yahoo.ro>
2023-10-21 16:33:31 +0300

    updated ChatGPT_test.py

--- ChatGPT_test.py
+++ ChatGPT_test.py
@@ -1,1 +1,13 @@
 import openai
+
+API_KEY = open("API_KEY.txt", 'r').read()
+openai.api_key = API_KEY
+
+response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "assistant", "content": "What is the general opinion on Katy Perry?"}
+    ]
+)
+
+print(response)
