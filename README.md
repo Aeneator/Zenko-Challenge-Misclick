b61bfce81d270e1320e7cced386fc94dcb0aedc1
Popa Bogdan-Gabriel <bogdan.popa.g25@gmail.com>
2023-10-21 15:45:58 +0300

    stand logic and readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# The-Zenko
-The Zenko Challenge.
+### Problem
+
+*What problem does your project solve? How does it fit into the theme "***An AI microsimulation***"?*
+
+### Solution
+
+*How did you resolve this issue? What technologies did you use? What was your biggest technical challenge and how does your solution solve it?*
+
+### Team and comments
+
+*Specify the name of your team (the same as on the discord/gather), and tell us what you learned during this hackathon!*
---
+++ read_data.py
@@ -0,0 +1,41 @@
+import json
+
+
+def get_stand_data():
+    shops_list = []
+    id_list = []
+    csv_file = open("RawFiles/export_stands20230922 (1).csv")
+    headers = next(csv_file)[:-1].split(';')
+    for row in csv_file:
+        row_data = row[:-1].split(';')
+        temp = {}
+        for i in range(1, len(headers)):
+            temp[headers[i]] = (row_data[i] if ', ' not in row_data[i] else row_data[i].split(', ')) if row_data[i] != '' else None
+        shops_list.append(temp)
+        id_list.append(row_data[0])
+
+    print(len(id_list))
+    print(len(set(id_list)))
+
+    json_file = open('RawFiles/fdv_stands20230920.geojson', "r")
+    stand_list = json.loads(json_file.read())['features']
+
+    index_list = [feature['properties']['numero'] for feature in stand_list]
+    for index in id_list:
+        try:
+            index_list.index(index)
+        except ValueError:
+            print(index)
+
+    for stand in stand_list:
+        try:
+            stand['properties']['details'] = shops_list[id_list.index(stand['properties']['numero'])]
+        except ValueError:
+            continue
+
+    json_object = json.dumps({'stand_list': stand_list}, indent=4)
+    with open("DataFiles/stand_data.json", "w") as outfile:
+        outfile.write(json_object)
+
+
+get_stand_data()
